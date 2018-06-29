d5a5a1b02cb36d77dbdbfd13307d7eb3abf2ea5d
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-06-29 11:33:31 +0200

    D

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var dbOperations = require("./psql.js");
 var logFmt = require("logfmt");
 app.set('views', __dirname + '/views') ;
 app.get('/' , function(req,res) {
-    res.sendfile('views/index.html');
+    res.sendfile('/index.html');
 } );
 app.get('/db/readRecords', function(req,res){
     dbOperations.getRecords(req,res);
@@ -18,12 +18,7 @@ app.get('/db/addRecord', function(req,res){
 app.get('/db/delRecord', function(req,res){
     dbOperations.delRecord(req,res);
 });
-app.get('/db/createTable', function(req,res){
-    dbOperations.createTable(req,res);
-});
-app.get('/db/dropTable', function(req,res){
-    dbOperations.dropTable(req,res);
-});
+
 app.set('port', process.env.PORT || 3001);
 app.use(express.static(__dirname + '/client'));
 app.listen(app.get('port'), function () {
