0c118be8b1125b959c8ba4ae69261c44e3e3bc5a
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-06-29 11:41:54 +0200

    F

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ app.get('/db/delRecord', function(req,res){
 });
 
 app.set('port', process.env.PORT || 3001);
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/public'));
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
