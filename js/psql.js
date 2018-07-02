58fb86ffa2aa3722cbe11ae0aaafaf1593726174
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-02 09:56:02 +0200

    AH

--- js/psql.js
+++ js/psql.js
@@ -4,7 +4,7 @@ module.exports = {
         //You can run command "heroku config" to see what is Database URL from Heroku belt
         var conString = "postgres://192.168.133.136:5432/"+req.query.db;
         var client = new pg.Client(conString);
-        await (client.connect());
+        /*await (client.connect());
         var query = await client.query("select * from "+req.query.table);
         res.rows.forEach(row=>{
           result.addRow(row);
@@ -12,7 +12,7 @@ module.exports = {
           res.write(JSON.stringify(result.rows, null, "    ") + "\n");
           res.end();
         });
-        await client.end();
+        await client.end();*/
 
   },
     addRecord : function(req, res){
@@ -20,14 +20,14 @@ module.exports = {
         var conString = process.env.DATABASE_URL ||  "postgres://postgres:Welcome123@localhost:5432/postgres";
         var client = new pg.Client(conString);
         client.connect();
-        /*var query = client.query("insert into employee (firstName,lastName,email,mobile) "+
+        var query = client.query("insert into employee (firstName,lastName,email,mobile) "+
                                 "values ('"+req.query.fName+"','"+req.query.lName+"','"+
                                     req.query.email+"','"+req.query.mbl+"')");
         query.on("end", function (result) {
             client.end();
             res.write('Success');
             res.end();
-        });*/
+        });
     },
      delRecord : function(req, res){
         var pg = require('pg');
