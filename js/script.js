3a841e6ce419248ea8700bab9f8dc4b6511abc50
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-04 17:06:46 +0200

    2018-07-04 17:06:45

--- js/script.js
+++ js/script.js
@@ -214,9 +214,9 @@ app.controller('addRowAreaController', function($scope, columnsDisplayFactory, p
 
     if(tableSelected != null){
       for(let i=0; i<$scope.foreignKeyList.length; i++){
-        if(att === $scope.foreignKeyList[i].column_name){
-          $scope.foreignColumName = $scope.foreignKeyList[i].foreign_column_name;
-          $scope.foreignTableName = $scope.foreignKeyList[i].foreign_table_name
+        if(att === postgresqlScope.columnConstraint[i].column_name){
+          $scope.foreignColumName = postgresqlScope.columnConstraint[i].foreign_column_name;
+          $scope.foreignTableName = postgresqlScope.columnConstraint[i].foreign_table_name;
           ret = true;
           break;
         }
