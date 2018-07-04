30fdffe0ecf38744ae4cf05106c2e9de0fb46ea8
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-04 11:22:06 +0200

    2018-07-04 11:22:06

--- auto_git/autoPull.sh
+++ auto_git/autoPull.sh
@@ -3,11 +3,11 @@ cd ..
 while true
 do
   (val=$(git pull)) &>/dev/null
+  echo $val
   if [ "$val" != "Déjà à jour." ]
   then
     echo "test"
     pkill -f node &>/dev/null
     (node app.js &) &>/dev/null
-    beep -n
   fi
 done
