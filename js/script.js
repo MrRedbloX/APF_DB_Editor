a0360b3905d6ac9690de4f835bfae094dec8480a
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-05 16:10:18 +0200

    2018-07-05 16:10:18

--- js/script.js
+++ js/script.js
@@ -87,7 +87,44 @@ app.controller('treeDatabaseAreaController', function($scope, postgresqlFactory)
               });
               $scope.ready = true;
               $(function() {
-                $('#treeDatabaseArea').jstree();
+                $('#treeDatabaseArea').jstree({
+                    "core" : {
+                      "animation" : 0,
+                      "check_callback" : true,
+                      "themes" : { "stripes" : true },
+                      'data' : {
+                        'url' : function (node) {
+                          return node.id === '#' ?
+                            'ajax_demo_roots.json' : 'ajax_demo_children.json';
+                        },
+                        'data' : function (node) {
+                          return { 'id' : node.id };
+                        }
+                      }
+                    },
+                    "types" : {
+                      "#" : {
+                        "max_children" : 1,
+                        "max_depth" : 4,
+                        "valid_children" : ["root"]
+                      },
+                      "root" : {
+                        "icon" : "Penguins.jpg",
+                        "valid_children" : ["default"]
+                      },
+                      "default" : {
+                        "valid_children" : ["default","file"]
+                      },
+                      "file" : {
+                        "icon" : "glyphicon glyphicon-file",
+                        "valid_children" : []
+                      }
+                    },
+                    "plugins" : [
+                      "contextmenu", "dnd", "search",
+                      "state", "types", "wholerow"
+                    ]
+                  });
               });
             }
           });
