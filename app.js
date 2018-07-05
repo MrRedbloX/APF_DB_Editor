c604a6fd0605e5ded3f792c515b51470a449a5ed
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-05 18:41:43 +0200

    2018-07-05 18:41:43

--- app.js
+++ app.js
@@ -30,13 +30,13 @@ app.get('/db/getValuesOf', function(req,res){
 app.get('/db/addRecord', function(req,res){
     dbOperations.addRecord(req,res);
 });
-app.get('/db/modifyRecord', function(req,res){
+/*app.get('/db/modifyRecord', function(req,res){
     dbOperations.modifyRecord(req,res);
 });
 app.get('/db/delRecord', function(req,res){
     dbOperations.delRecord(req,res);
 });
-
+*/
 app.set('port', process.env.PORT || 3001);
 app.use(express.static(__dirname));
 app.listen(app.get('port'), function () {
