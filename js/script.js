6b32d33bba4808af477003f7dba90994710aeb46
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-06 10:17:03 +0200

    2018-07-06 10:17:03

--- js/script.js
+++ js/script.js
@@ -286,7 +286,7 @@ app.controller('addRowAreaController', function($scope, columnsDisplayFactory, p
 
         valueList = [];
         for(col in columnList){
-          console.log(col);
+          console.log(columnList[0]);
           let elt = document.getElementById(col);
           if(elt.nodeName === "INPUT") valueList.push(elt.value);
           else if(elt.nodeName === "SELECT") value.list.push(elt.options[elt.selectedIndex].text);
