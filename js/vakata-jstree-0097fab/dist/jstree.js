a8d11192fc9cce637fd2c5c45b73397c36b94694
aloisguitton <aloisguitton@orange.fr>
2018-07-06 09:57:07 +0200

    2018-07-06 09:57:07

--- js/vakata-jstree-0097fab/dist/jstree.js
+++ js/vakata-jstree-0097fab/dist/jstree.js
@@ -8530,7 +8530,7 @@
 						var tmp = $.Event('click', { metaKey : e.metaKey, ctrlKey : e.ctrlKey, altKey : e.altKey, shiftKey : e.shiftKey });
 						$(e.currentTarget).closest(".jstree-node").children(".jstree-anchor").first().trigger(tmp).focus();
 					}, this))
-				.on("mouseover.jstree", ".jstree-wholerow, .jstree-icon", $.proxy(function (e) {
+				.on("mouseover.jstree", ".jstree-wholerow", $.proxy(function (e) {
 						e.stopImmediatePropagation();
 						if(!this.is_disabled(e.currentTarget)) {
 							this.hover_node(e.currentTarget);
