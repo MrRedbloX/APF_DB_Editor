0cd36b4c5cb9fde7ceba3a33315d5a1b49bb0a34
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-06 17:15:24 +0200

    2018-07-06 17:15:24

--- js/script.js
+++ js/script.js
@@ -364,9 +364,7 @@ app.controller('modifyRowAreaController', function($scope, columnsDisplayFactory
                 buttonAreaScope.display();
                 valueList.unshift(pkValue);
                 document.getElementById(rowSelected).id = JSON.stringify(valueList);
-                console.log(document.getElementById(JSON.stringify(valueList)));
                 rowSelected = JSON.stringify(valueList);
-                console.log(rowSelected);
               }
             });
           }
