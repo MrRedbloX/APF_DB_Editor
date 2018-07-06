bc27c497275eb7ed9b345a271177b8f50cb848b0
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-06 14:15:51 +0200

    2018-07-06 14:15:51

--- js/script.js
+++ js/script.js
@@ -317,7 +317,7 @@ app.controller('modifyRowAreaController', function($scope, columnsDisplayFactory
   var columnsDisplayScope = columnsDisplayFactory.getScope();
   var postgresqlScope = postgresqlFactory.getScope();
 
-  console.log(postgresqlFactory.tableArray);
+  console.log(postgresqlScope.tableArray);
 
   $scope.attributes = [];
   let parseRowSelected = JSON.parse(rowSelected);
