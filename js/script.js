20069ee7267a9177908da3e515da443d933a0b90
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-09 14:56:00 +0200

    2018-07-09 14:56:00

--- js/script.js
+++ js/script.js
@@ -472,13 +472,13 @@ app.controller('modifyRowAreaController', function($scope, columnsDisplayFactory
       for(let i=0; i<postgresqlScope.valuesOfConstraint.length; i++){
         if(att === postgresqlScope.valuesOfConstraint[i].name){
           for(let j=0; j<postgresqlScope.valuesOfConstraint[i].values.length; j++)
-              $scope.references.push(postgresqlScope.valuesOfConstraint[i].values[j]);      
+              $scope.references.push(postgresqlScope.valuesOfConstraint[i].values[j]);
           $scope.references.splice($scope.references.indexOf(val), 1);
           break;
         }
       }
     }
-    return ret;
+    return $scope.references;
   };
 });
 
