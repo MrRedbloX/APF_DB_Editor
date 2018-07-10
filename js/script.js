c3e3ea6b90173e2f72d4216a191a6a71015c9be2
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-10 14:44:16 +0200

    2018-07-10 14:44:16

--- js/script.js
+++ js/script.js
@@ -422,14 +422,14 @@ app.controller('modifyRowAreaController', function($scope, columnsDisplayFactory
         for(let j=0; j<columnList.length; j++){
           let elt = document.getElementById(columnList[j]);
           if(elt.nodeName === "INPUT"){
-            if(elt.value == null || elt.options[elt.selectedIndex] == null) valueList.push(" ");
+            if(elt.value == null) valueList.push(" ");
             else{
               if($scope.attributes[j].name.data_type.toLowerCase().includes("int")) valueList.push(parseInt(elt.value));
               else valueList.push(elt.value);
             }
           }
           else if(elt.nodeName === "SELECT"){
-            if(elt.selectedIndex == null) valueList.push(" ");
+            if(elt.selectedIndex == null || elt.options[elt.selectedIndex] == null) valueList.push(" ");
             else{
                if($scope.attributes[j].name.data_type.toLowerCase().includes("int")) valueList.push(parseInt(elt.options[elt.selectedIndex].text));
                else valueList.push(elt.options[elt.selectedIndex].text);
