2b7138feae712486729300130b8e46456d59e85d
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-11 15:15:58 +0200

    2018-07-11 15:15:57

--- js/script.js
+++ js/script.js
@@ -246,7 +246,7 @@ app.controller('buttonAreaController', function($scope, columnsDisplayFactory, p
               for(let i=0;i<postgresScope.columnValues.data.length;i++){
                 temp = [];
                 for(let j=0;j<columnsDisplayScope.columns.length;j++){
-                  temp.push(postgresScope.columnValues.data[i][(columnsDisplayScope.columns[j].column_name)/*.toString()*/]);
+                  temp.push(postgresScope.columnValues.data[i][(columnsDisplayScope.columns[j].column_name)]);
                 }
                 columnsDisplayScope.tuples.push({
                   column_names : columnsDisplayScope.columns,
