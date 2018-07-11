d5093a6dd3cc37b2e8cf0779e7c8816b872dafb7
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-11 17:59:26 +0200

    2018-07-11 17:59:26

--- js/script.js
+++ js/script.js
@@ -175,6 +175,7 @@ app.controller('columnsDisplayAreaController', function($scope, columnsDisplayFa
   };
 
   $scope.setToolTips = function(){
+    console.log("tooltips");
     for(let i=0; i<$scope.row_ids.length; i++){
       if($scope.checkIfIsReference($scope.row_ids[i].column_name)){
         if(document.getElementById($scope.row_ids[i].id) != null) document.getElementById($scope.row_ids[i].id).title = $scope.getInfoForFK($scope.row_ids[i].column_name,$scope.row_ids[i].value);
