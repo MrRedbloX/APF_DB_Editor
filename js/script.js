e6e31b5c6d4e064cc968dd726256e072852b2fda
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-11 17:59:46 +0200

    2018-07-11 17:59:46

--- js/script.js
+++ js/script.js
@@ -495,7 +495,6 @@ app.controller('addRowAreaController', function($scope, columnsDisplayFactory, p
   };
 
   $scope.setIdForToolTips = function(val, column){
-    console.log("ok");
     let theID = "a"+column+val;
     columnsDisplayScope.row_ids.push({
       id : theID,
