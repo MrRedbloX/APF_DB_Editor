f2bcf5f1ea2473fcdbb65b026070e957306bc38d
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-11 15:47:21 +0200

    2018-07-11 15:47:21

--- js/script.js
+++ js/script.js
@@ -181,6 +181,7 @@ app.controller('columnsDisplayAreaController', function($scope, columnsDisplayFa
     console.log(postgresqlScope.valuesOfConstraint);
     for(let i=0; i<postgresqlScope.valuesOfConstraint.length; i++){
       if(column_name === postgresqlScope.valuesOfConstraint[i].name){
+        console.log(postgresqlScope.valuesOfConstraint[i]);
         for(val in postgresqlScope.valuesOfConstraint[i].values){//let j=0; j<postgresqlScope.valuesOfConstraint[i].values.length; j++
           console.log(postgresqlScope.valuesOfConstraint[i].values[val].id+" vs "+value);
           if(postgresqlScope.valuesOfConstraint[i].values[val].id === value){
