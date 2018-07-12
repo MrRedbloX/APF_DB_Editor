a63550c1bc4e4e4470a4b8ac68cdb6cefa4661d5
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-12 19:16:16 +0200

    2018-07-12 19:16:16

--- js/script.js
+++ js/script.js
@@ -473,7 +473,7 @@ app.controller('buttonAreaController', function($scope, columnsDisplayFactory, p
               console.log("Found db");
               for(let k=0; k<treeDatabaseAreaScope.databases[j].table.length; k++){
                 if(treeDatabaseAreaScope.databases[j].table[k] != table){
-                  console.log("Found table");
+                  console.log(treeDatabaseAreaScope.databases[j].table[k]);
                   postgresScope.getColumnConstraint(db, treeDatabaseAreaScope.databases[j].table[k], function(){
                     if(postgresScope.successRequest){
                       for(let l=0; l<postgresScope.columnConstraint.length; l++){
