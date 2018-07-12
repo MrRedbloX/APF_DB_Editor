9dd70830f90ad75f80d76d4260c462ad1988cf80
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-12 19:10:59 +0200

    2018-07-12 19:10:58

--- js/script.js
+++ js/script.js
@@ -461,8 +461,8 @@ app.controller('buttonAreaController', function($scope, columnsDisplayFactory, p
 
       postgresScope.getPrimaryKey(db, table, function(){
         if(postgresScope.successRequest){
-          console.log("get pk success");
           for(let i=0; i<columnsDisplayScope.columns.length; i++){
+            console.log(postgresScope.primaryKey.data[0].attname+" vs "+columnsDisplayScope.columns[i]);
             if(postgresScope.primaryKey.data[0].attname == columnsDisplayScope.columns[i]){
               console.log("Found pk");
               var pkValue = JSON.parse(currentRowSelected)[i];
