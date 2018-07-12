b08e2aacce85e6cf11adc5614ca2126522612f07
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-12 10:57:59 +0200

    2018-07-12 10:57:59

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var app = angular.module('DBEditorAPF', ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "html/database_management/db_management.html"
+    })
+});
