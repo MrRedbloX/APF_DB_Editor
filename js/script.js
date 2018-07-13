f8ab539885e1a433750e485cff5b1ac77150cdba
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-13 14:28:02 +0200

    2018-07-13 14:28:02

--- js/script.js
+++ js/script.js
@@ -1011,7 +1011,7 @@ app.controller('relationsAreaController', function($scope, postgresqlFactory, co
                                 for(let i=0; i<$scope.relationsData.length; i++)
                                   $scope.tables.push($scope.relationsData[i].table_name);
 
-                                console.log($scope.table.length);
+                                console.log($scope.tables.length);
 
                                 $scope.ready = true;
                               }
