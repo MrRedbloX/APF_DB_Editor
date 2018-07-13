adeac6390a454d1aa80a5425f1cbb03bab72df73
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-13 14:15:30 +0200

    2018-07-13 14:15:29

--- js/script.js
+++ js/script.js
@@ -1031,6 +1031,7 @@ app.controller('relationsAreaController', function($scope, postgresqlFactory, co
         }
         busy = false;
         if(document.getElementById("showRelationsButton") != null) document.getElementById("showRelationsButton").disabled = false;
+        console.log($scope.relationsData);
         for(let i=0; i<$scope.relationsData.data.length; i++)
           $scope.table.push($scope.relationsData[i].table_name);
 
