2918c1afc5021ea8d396508e93e2ebf0446acf6a
aloisguitton <aloisguitton@orange.fr>
2018-07-16 16:10:08 +0200

    2018-07-16 16:10:08

--- app2.js
+++ app2.js
@@ -45,7 +45,7 @@ app.get('/db/query', function(req,res){
     dbOperations.query(req,res);
 });
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3002);
 app.use(express.static(__dirname));
 app.listen(app.get('port'), function () {
     //console.log('Express server listening on port ' + app.get('port'));
