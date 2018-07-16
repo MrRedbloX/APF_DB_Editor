aa7fbcc04b9062f00378776ab7e47eb0574b944a
aloisguitton <aloisguitton@orange.fr>
2018-07-16 16:29:24 +0200

    2018-07-16 16:29:24

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-  $conf=pg_connect("host=10.237.169.202 dbname=test user=postgres password=postgres port=5432"):
+  $conf=pg_connect("host=10.237.169.202 dbname=test user=postgres password=postgres port=5432");
   if($conf){
     echo "success";
   }
