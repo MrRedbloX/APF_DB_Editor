fdfa989a87eeac2071bc5f757c0f10f039255404
aloisguitton <aloisguitton@orange.fr>
2018-07-16 15:43:18 +0200

    2018-07-16 15:43:18

--- js/id.js
+++ js/id.js
@@ -31,8 +31,29 @@ function iden() {
 }
 
 function sqlfun(){
-  var conString = "postgres://postgres:postgres@10.237.169.202:5432/";
-  var pg = require('pg');
+  getDBName: function(req, res) {
+        var pg = require('pg');
+
+        var client = new pg.Client(conString+"postgres");
+
+        client.connect(function(err,client) {
+          if(err){
+           console.log("Not able to get connection : "+ err);
+           res.status(400).send(err);
+          }
+          else{
+            console.log("Connection successful");
+            client.query("SELECT datname FROM pg_database ORDER BY datname;" ,function(err,result) {
+              client.end(); // closing the connection;
+              if(err){
+                 console.log(err);
+                 res.status(400).send(err);
+              }
+              else res.status(200).send(result.rows);
+            });
+          }
+        });
+  }
 }
 
 function clear_cache(){
