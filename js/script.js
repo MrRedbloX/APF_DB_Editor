201d74c327e463b9b95ac31d4f778e0697ab93b7
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-17 16:37:53 +0200

    2018-07-17 16:37:53

--- js/script.js
+++ js/script.js
@@ -279,6 +279,7 @@ app.controller('columnsDisplayAreaController', function($scope, columnsDisplayFa
     for(let i=0; i<$scope.tuples.length; i++){
       for(let j=0; j<$scope.tuples[i].values.length; j++){
         str = fk_column_name+JSON.stringify($scope.tuples[i].values)+$scope.tuples[i].values[j];
+        if(document.getElementById(str) != null) console.log("YES");
         postgresqlScope.query(db, table, theName, pk, $scope.tuples[i].values[j], function(){
           if(postgresqlScope.successRequest){
             console.log(postgresqlScope.queryRequest);
