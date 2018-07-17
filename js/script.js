419cd52a52d8b44ea3a2616a162d0453dd6c443f
aloisguitton <aloisguitton@orange.fr>
2018-07-17 15:57:06 +0200

    2018-07-17 15:57:06

--- js/script.js
+++ js/script.js
@@ -1194,7 +1194,7 @@ app.controller('loginController', function($scope, postgresqlFactory){
         alert("Error on getIdFromMD5 request, check console logs.");
       }
     });
-    console.log("existe : "$scope.id_exist)
+    console.log("existe : "$scope.id_exist);
     return false;
   }
 });
