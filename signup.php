8129b01969f5eb711c701dff077791db74070c93
aloisguitton <aloisguitton@orange.fr>
2018-07-17 09:34:08 +0200

    2018-07-17 09:34:08

--- signup.php
+++ signup.php
@@ -32,9 +32,9 @@
 
       $db = pg_connect( "$host $port $dbname $credentials"  );
       if(!$db) {
-         echo "Error : Unable to open database\n";
+         echo "Error : Unable to open database";
       } else {
-         echo "Opened database successfully\n";
+         echo "Opened database successfully";
       }
     }
   ?>
