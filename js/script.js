8f04fafaeee2254d5a8a214b062bb8620219a4f8
aloisguitton <aloisguitton@orange.fr>
2018-07-17 11:54:46 +0200

    2018-07-17 11:54:46

--- js/script.js
+++ js/script.js
@@ -1186,27 +1186,7 @@ app.controller('signupController', function($scope){
   var conString = "postgres://postgres:postgres@10.237.169.202:5432/";
 
   $scope.getDBName = function() {
-    var pg = require('pg');
-
-    var client = new pg.Client(conString+"test");
-
-    client.connect(function(err,client) {
-      if(err){
-       console.log("Not able to get connection : "+ err);
-       res.status(400).send(err);
-      }
-      else{
-        console.log("Connection successful");
-        client.query("SELECT datname FROM pg_database ORDER BY datname;" ,function(err,result) {
-          client.end(); // closing the connection;
-          if(err){
-             console.log(err);
-             res.status(400).send(err);
-          }
-          else res.status(200).send(result.rows);
-        });
-      }
-    });
+    
   }
 
 });
