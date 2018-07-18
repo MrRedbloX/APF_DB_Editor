64c34d2ce48024d7389cad9cc6dc74ae69c19a5f
aloisguitton <aloisguitton@orange.fr>
2018-07-18 10:25:29 +0200

    2018-07-18 10:25:29

--- js/script.js
+++ js/script.js
@@ -1249,13 +1249,14 @@ app.controller('loginController', function($scope, postgresqlFactory){
 
   $scope.readCookie = function(name) {
 	var nameEQ = name + "=";
+  var cook = "";
 	var ca = document.cookie.split(';');
 	for(var i=0;i < ca.length;i++) {
 		var c = ca[i];
 		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		if (c.indexOf(nameEQ) == 0) cook = c.substring(nameEQ.length,c.length);
 	}
-	return null;
+	alerte
 }
 
   $scope.createCookie = function(name,value,days) {
