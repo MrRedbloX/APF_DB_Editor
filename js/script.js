e8a84636ee5d1760356cc35dff8a1add27d7c094
aloisguitton <aloisguitton@orange.fr>
2018-07-18 10:23:45 +0200

    2018-07-18 10:23:45

--- js/script.js
+++ js/script.js
@@ -1247,6 +1247,17 @@ app.controller('loginController', function($scope, postgresqlFactory){
     });
   }
 
+  $scope.readCookie = function(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
   $scope.createCookie = function(name,value,days) {
 	if (days) {
 		var date = new Date();
