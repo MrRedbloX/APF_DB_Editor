2dcb2718894f7f3af12ba55daec31a5dbae1cdec
aloisguitton <aloisguitton@orange.fr>
2018-07-18 11:00:14 +0200

    2018-07-18 11:00:14

--- js/loginControllers.js
+++ js/loginControllers.js
@@ -100,6 +100,22 @@ app.controller('loginController', function($scope, postgresqlFactory){
     });
   }
 
+  function readCookie(name) {
+    var name = cname + "=";
+    cook = "";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+  }
+
   $scope.createCookie = function(name,value,days) {
 	if (days) {
 		var date = new Date();
