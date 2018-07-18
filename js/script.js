6ac743c4367f224a865b8bc1f8ef145c3b0fff51
aloisguitton <aloisguitton@orange.fr>
2018-07-18 09:17:14 +0200

    2018-07-18 09:17:14

--- js/script.js
+++ js/script.js
@@ -1231,7 +1231,7 @@ app.controller('loginController', function($scope, postgresqlFactory){
 
       if($scope.id_exist == true){
         if(rm.checked == true){
-          document.cookie = "id="+md5;
+          document.cookie = "id=md5";
         }
         window.location="/";
         localStorage['apf_project_db_editor_login'] = '1';
