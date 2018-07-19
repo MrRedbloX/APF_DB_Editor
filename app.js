bc320228c0f71827cf9edab16f76f4898799d302
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-19 17:11:27 +0200

    2018-07-19 17:11:26

--- app.js
+++ app.js
@@ -53,6 +53,11 @@ app.get('/db/addLogin', function(req,res){
 
 app.set('port', process.env.PORT || 3001);
 app.use(express.static(__dirname));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.listen(app.get('port'), function () {
     //console.log('Express server listening on port ' + app.get('port'));
 });
