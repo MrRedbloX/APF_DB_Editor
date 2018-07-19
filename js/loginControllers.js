b03d6fb2343de3be41a6e7a4f7a07d00520bcf32
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-19 17:08:52 +0200

    2018-07-19 17:08:52

--- js/loginControllers.js
+++ js/loginControllers.js
@@ -240,12 +240,12 @@ app.controller('signupController', function($scope, $http, postgresqlFactory){
       $http({
         method: 'GET',
         url: lien,
-        dataType: 'jsonp',
-        headers: {
+        dataType: 'jsonp'
+        /*headers: {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods' : 'GET',
           'Access-Control-Allow-Headers' : 'Origin, Content-Type, X-Auth-Token'
-        }
+        }*/
       })
       .then(
         function successCallback(data) {
