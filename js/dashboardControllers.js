5be822f54440772f8c4b8a9a5e535e3d0b5cf7c2
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-19 15:12:19 +0200

    2018-07-19 15:12:19

--- js/dashboardControllers.js
+++ js/dashboardControllers.js
@@ -43,6 +43,7 @@ app.controller('chartDisplayController', function($scope, postgresqlFactory){
       data.push($scope.databases[i].table.length);
     }
 
+    console.log($scope.databases);
     console.log(labels);
     console.log(data);
 
