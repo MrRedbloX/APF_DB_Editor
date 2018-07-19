62b511228baa75dbb0d13af46e5025d173f868ef
aloisguitton <aloisguitton@orange.fr>
2018-07-19 15:46:42 +0200

    2018-07-19 15:46:42

--- js/loginControllers.js
+++ js/loginControllers.js
@@ -185,7 +185,7 @@ app.controller('signupController', function($scope, postgresqlFactory){
     console.log(lien.innerHTML);
 
     window.location = lien;
-    console.log(document.getElementsByTagName("h3"));
+    //console.log(document.getElementsByTagName("h3"));
   //  document.getElementById('annuaire').innerHTML = '<iframe src="' + lien + '" width="640" height="480" ></iframe>';
 
     //$scope.verif_user(2000);
