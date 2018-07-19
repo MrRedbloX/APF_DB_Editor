b699d3af1b19a75a869370ec628969be8d3fd899
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-19 15:23:09 +0200

    2018-07-19 15:23:09

--- js/dashboardControllers.js
+++ js/dashboardControllers.js
@@ -17,7 +17,7 @@ app.controller('chartDisplayController', function($scope, postgresqlFactory){
                     name : postgresScope.dbArray.data[i].datname,
                     table : postgresScope.tableArray.data
                   });
-                  //console.log(i.toString());
+                  console.log(index.toString());
                   //console.log(postgresScope.dbArray.data.length.toString());
                   console.log(postgresScope.dbArray.data);
                   if(index == postgresScope.dbArray.data.length-1) $scope.ready = true;
