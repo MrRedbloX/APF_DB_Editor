1ccea4043726f7d001e131218c08adb17c25d59b
aloisguitton <aloisguitton@orange.fr>
2018-07-19 09:58:10 +0200

    2018-07-19 09:58:10

--- js/loginControllers.js
+++ js/loginControllers.js
@@ -183,7 +183,7 @@ app.controller('signupController', function($scope, postgresqlFactory){
     lien = "annuaire.sso.infra.ftgroup/persons?searchType=PERSON_COMPLEX&personCriteria.cru=&personCriteria.alphabetical=false&personCriteria.sn="+nom+"&personCriteria.snSelect=STARTSWITH&personCriteria.givenName="+prenom+"&personCriteria.givenNameSelect=STARTSWITH&_personCriteria.usePhonetic=on&personCriteria.telephoneNumber=&personCriteria.mail="+mail_lien+"&personCriteria.ftadmou=&personCriteria.ftsubactivitiescode=&personCriteria.ftactivitiescode=&personCriteria.ftskill=&personCriteria.ftactivities=&personCriteria.siteCriteria.ville=&personCriteria.siteCriteria.site=&personCriteria.siteCriteria.codePostDep=*";
 
     console.log(lien);
-    console.log(lien.getElementsByTagName("h3"));
+    console.log(document.getElementsByTagName("h3"));
 /*
     console.log("user " + user + " mail " + mail + " mdp " + pass + " md5 " + result);
     postgresScope.addLogin(user,result,mail,function(){
