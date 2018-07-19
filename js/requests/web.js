186f524c366735974dbead13cb2ec033d3e43c94
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-19 18:18:55 +0200

    2018-07-19 18:18:55

--- js/requests/web.js
+++ js/requests/web.js
@@ -3,7 +3,7 @@ module.exports = {
   getAnnuaire: function(req, res){
 
     var options = {
-      hostname: "https://google.com",
+      hostname: "google.com",
       method: 'GET',
       headers: {
           'Content-Type': 'application/json',
