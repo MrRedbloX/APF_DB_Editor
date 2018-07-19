865c24355642d4f551eb9a8184d81326b1aed8de
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-19 11:43:40 +0200

    2018-07-19 11:43:40

--- js/databasesControllers.js
+++ js/databasesControllers.js
@@ -988,13 +988,14 @@ app.controller('relationsAreaController', function($scope, postgresqlFactory, co
                       //console.log(postgresScope.columnConstraint.data[l].foreign_table_name+" vs "+table);
                       console.log(postgresScope.columnConstraint.data[l].foreign_column_name+" vs "+pkName)
                       if(postgresScope.columnConstraint.data[l].foreign_table_name == table && postgresScope.columnConstraint.data[l].foreign_column_name == pkName){
+                        col = postgresScope.columnConstraint.data[l].column_name;
                         //console.log("Conditions vérifiées");
                         //console.log("Get pk de : "+postgresScope.columnConstraint.data[l].table_name);
                         postgresScope.getPrimaryKey(db, postgresScope.columnConstraint.data[l].table_name, function(){
                           if(postgresScope.successRequest){
                             //console.log("PK : "+postgresScope.primaryKey.data[0].attname);
                             console.log("SELECT * FROM "+treeDatabaseAreaScope.databases[j].table[k].table_name+" WHERE "+postgresScope.columnConstraint.data[l].column_name+" = "+pkValue);
-                            postgresScope.query(db, treeDatabaseAreaScope.databases[j].table[k].table_name, "*", postgresScope.columnConstraint.data[l].column_name, pkValue, function(){
+                            postgresScope.query(db, treeDatabaseAreaScope.databases[j].table[k].table_name, "*", col, pkValue, function(){
                               if(postgresScope.successRequest){
                                 //console.log("Table : "+postgresScope.columnConstraint.data[l].table_name);
                                 console.log(postgresScope.queryRequest.data);
