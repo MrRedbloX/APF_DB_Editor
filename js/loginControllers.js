02999f3bc47fbc1a35a6f5d15ea73ac2540065a5
aloisguitton <aloisguitton@orange.fr>
2018-07-19 16:13:44 +0200

    2018-07-19 16:13:43

--- js/loginControllers.js
+++ js/loginControllers.js
@@ -184,23 +184,7 @@ app.controller('signupController', function($scope, postgresqlFactory){
 
     console.log(lien);
 
-    function makeHttpObject() {
-      try {return new XMLHttpRequest();}
-      catch (error) {}
-      try {return new ActiveXObject("Msxml2.XMLHTTP");}
-      catch (error) {}
-      try {return new ActiveXObject("Microsoft.XMLHTTP");}
-      catch (error) {}
-
-      throw new Error("Could not create HTTP request object.");
-    }
-    var request = makeHttpObject();
-    request.open("GET", lien, true);
-    request.send(null);
-    request.onreadystatechange = function() {
-      if (request.readyState == 4)
-        alert(request.responseText);
-    };
+    var wget = require('wget');
     //$scope.verif_user(2000);
 
 /*
