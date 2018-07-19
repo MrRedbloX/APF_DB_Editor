4f7d2ff5173306923b0ba3b05d3f1472ec2691df
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-19 10:58:01 +0200

    2018-07-19 10:58:01

--- js/databasesControllers.js
+++ js/databasesControllers.js
@@ -993,7 +993,7 @@ app.controller('relationsAreaController', function($scope, postgresqlFactory, co
                         postgresScope.getPrimaryKey(db, postgresScope.columnConstraint.data[l].table_name, function(){
                           if(postgresScope.successRequest){
                             //console.log("PK : "+postgresScope.primaryKey.data[0].attname);
-                            //console.log("SELECT * FROM "+postgresScope.columnConstraint.data[l].table_name+" WHERE "+postgresScope.columnConstraint.data[l].column_name+" = "+pkValue);
+                            console.log("SELECT * FROM "+postgresScope.columnConstraint.data[l].table_name+" WHERE "+postgresScope.columnConstraint.data[l].column_name+" = "+pkValue);
                             postgresScope.query(db, postgresScope.columnConstraint.data[l].table_name, "*", postgresScope.columnConstraint.data[l].column_name, pkValue, function(){
                               if(postgresScope.successRequest){
                                 console.log(l);
