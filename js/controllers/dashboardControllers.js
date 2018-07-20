97a29bf462684e7c215236869aa4d79793b5e5bd
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-20 11:27:47 +0200

    2018-07-20 11:27:47

--- js/controllers/dashboardControllers.js
+++ js/controllers/dashboardControllers.js
@@ -31,8 +31,9 @@ app.controller('chartDisplayController', function($scope, postgresqlFactory){
               if(postgresScope.successRequest){
                 //console.log(tables);
                 for(let j=0; j<postgresScope.tableArray.data.length; j++){
+                  console.log("fonction "+j);
                   postgresScope.getAllValues(db[i], postgresScope.tableArray.data[j].table_name, function(){
-                    console.log("Get "+db[i]+" "+postgresScope.tableArray.data[j].table_name);
+                    console.log("callback "+j);
                     if(postgresScope.successRequest){
                       //console.log(j);
                       //console.log(tables.length);
