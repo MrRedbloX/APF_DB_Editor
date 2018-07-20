1485103533505b0d3dce08a5dd32f4d927049ee8
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-20 14:29:29 +0200

    2018-07-20 14:29:29

--- js/controllers/dashboardControllers.js
+++ js/controllers/dashboardControllers.js
@@ -59,6 +59,7 @@ app.controller('chartDisplayController', function($scope, postgresqlFactory){
           postgresScope.getAllValues($scope.databases[i].name, $scope.databases[i].table[j].table_name, function(){
             if($scope.successRequest){
               $scope.tables.push({
+                db : $scope.databases[i].name,
                 name : $scope.databases[i].table[j].table_name,
                 values : postgresScope.columnValues
               });
