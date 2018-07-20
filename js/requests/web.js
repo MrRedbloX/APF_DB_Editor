5766feaea348281070bf38b20431b87cd339e55a
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-20 10:01:23 +0200

    2018-07-20 10:01:23

--- js/requests/web.js
+++ js/requests/web.js
@@ -32,44 +32,5 @@ module.exports = {
     });
 
     request.end();
-
-    /*if (window.XMLHttpRequest) {
-        // code for modern browsers
-        xmlhttp = new XMLHttpRequest();
-     } else {
-        // code for old IE browsers
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        res.status(200).send(this.responseText);
-      }
-      else{
-        res.status(400).send(this.responseText);
-      }
-    };
-    xhttp.open("GET", req.lien , true);
-    xhttp.send();
-
-    /*$.ajax({
-      method: 'GET',
-      url: req.lien,
-      dataType: 'jsonp',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods' : 'GET',
-        'Access-Control-Allow-Headers' : 'Origin, Content-Type, X-Auth-Token'
-      }
-    })
-    .then(
-      function successCallback(data) {
-        console.log(data);
-        res.status(200).send(data);
-      },
-      function errorCallback(data) {
-        console.log(data);
-        res.status(400).send(data);
-    });*/
   }
 }
