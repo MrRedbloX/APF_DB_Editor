4d7bcfaf826831ff77faecd83420fa018724fb08
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-23 10:42:04 +0200

    2018-07-23 10:42:03

--- js/controllers/dashboardControllers.js
+++ js/controllers/dashboardControllers.js
@@ -190,6 +190,8 @@ app.controller('chartDisplayController', function($scope, postgresqlFactory){
             }
         }
       });
+
+      $scope.readyLoadNbTuplesInTable = false;
     }
   };
 });
