4e6941dc2ae4307755f664fab6fdb3332a1bfad1
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-23 10:52:00 +0200

    2018-07-23 10:52:00

--- js/controllers/dashboardControllers.js
+++ js/controllers/dashboardControllers.js
@@ -121,9 +121,9 @@ app.controller('chartDisplayController', function($scope, postgresqlFactory){
   };
 
   $scope.loadChartNbTuplesInTable = function(db){
-    test = document.getElementsByTagName("CANVAS");
-    console.log(test);
-    var ctx = document.getElementById(db);
+    canvas = document.getElementById("nbTuplesInTable");
+    canvas.id = canvas.id+db;
+    var ctx = document.getElementById("nbTuplesInTable"+db);
     console.log(ctx);
     var labels = [];
     var data = [];
