657feef33c5c9e5f24408cdb530226733b08ed38
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-23 15:34:23 +0200

    2018-07-23 15:34:23

--- public/js/controllers/dashboardControllers.js
+++ public/js/controllers/dashboardControllers.js
@@ -21,6 +21,7 @@ app.controller('chartDisplayController', function($scope, postgresqlFactory){
   $scope.splitTheTableName = function(table){
     return (table.split('_'))[0];
   }
+  
   $scope.loadDB = function(){
     if(!$scope.readyDB){
       postgresScope.getDBName(function(){ //We do the request and we define the callback function
