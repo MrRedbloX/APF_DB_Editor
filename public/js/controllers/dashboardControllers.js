126338d874446711a52d0ce83fa5283b009138ee
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-23 15:40:14 +0200

    2018-07-23 15:40:14

--- public/js/controllers/dashboardControllers.js
+++ public/js/controllers/dashboardControllers.js
@@ -138,7 +138,7 @@ app.controller('chartDisplayController', function($scope, postgresqlFactory){
     //console.log($scope.tables);
     console.log(db);
 
-    for(let i=0; i<$scope.tables.length; i++){
+    for(let i=0; i<$scope.databases.length; i++){
       if($scope.tables[i].db == db && !isInExceptionTables($scope.tables[i].name)){
         for(let j=0; j<$scope.dbColors.length; j++){
           if($scope.tables[i].db == $scope.dbColors[j].db_name)
