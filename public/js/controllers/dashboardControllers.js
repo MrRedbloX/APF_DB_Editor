296de7492a2760a374814324ee7314bac2bbc43c
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-25 13:26:00 +0200

    2018-07-25 13:26:00

--- public/js/controllers/dashboardControllers.js
+++ public/js/controllers/dashboardControllers.js
@@ -11,6 +11,7 @@ app.controller('chartDisplayController', function($scope, postgresqlFactory, but
   $scope.dbColors = [];
   $scope.databases = [];
   $scope.chartsSondeTenant = [];
+  $scope.relTenantIdName = [];
 
   $scope.getRGBA = function(){
     ret = [];
