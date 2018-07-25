b51f3d75c01736607d65e841326e4bb26a19d5d1
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-25 11:44:22 +0200

    2018-07-25 11:44:21

--- public/js/controllers/dashboardControllers.js
+++ public/js/controllers/dashboardControllers.js
@@ -317,7 +317,6 @@ app.controller('chartDisplayController', function($scope, postgresqlFactory, but
 
     canvas.onclick = function(evt){
       var activePoints = myChart.getElementsAtEvent(evt);
-      console.log(activePoints);
       if(activePoints.length > 0){
         window.location = "/#!/db_management";
         $scope.wait();
