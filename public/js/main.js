3213c8fb9ac6eeeca8cfadfcca4dd132f2bae3ba
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-25 16:18:25 +0200

    2018-07-25 16:18:25

--- public/js/main.js
+++ public/js/main.js
@@ -77,6 +77,7 @@ function sleep(ms) {
 function containsForbiddenChar(str){
   ret = false;
   for(let i=0; i<forbiddenChar.length; i++){
+    console.log(str+" vs "+forbiddenChar[i]);
     if(str.indexOf(forbiddenChar[i]) > -1){
       ret = true;
       break;
