c0f4f1bf17f82f2afc17860bc8024fff6c829cf3
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-26 15:34:29 +0200

    2018-07-26 15:34:29

--- server/js/requests/web.js
+++ server/js/requests/web.js
@@ -1,8 +1,6 @@
 var http = require("http");
 module.exports = {
   getAnnuaire: function(req, res){
-
-    console.log(req.query.lien);
     lien = req.query.lien+"&personCriteria.sn="+req.query.personCriteriaN+"&personCriteria.givenName="+req.query.personCriteriaP+"&personCriteria.mail="+req.query.personCriteriaM;
 
     http.get(lien, (resp) => {
