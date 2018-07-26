e801dc7562ae7b96a7b1c4de14617808a541be98
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-26 14:54:51 +0200

    2018-07-26 14:54:51

--- server/js/requests/web.js
+++ server/js/requests/web.js
@@ -2,45 +2,19 @@ var http = require("http");
 module.exports = {
   getAnnuaire: function(req, res){
 
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if(xmlHttp.readyState == 4 && xmlHttp.status == 200)
-          res.status(200).send(xmlHttp.responseText);
-        else
-          res.status(400).send("Wrong URL");
-    }
-    xmlHttp.open("GET", req.query.lien, true); // true for asynchronous
-    xmlHttp.send(null);
+    http.get(req.query.lien, (resp) => {
+      let data = '';
 
-    /*var options = {
-      hostname: req.query.lien,
-      method: 'GET',
-      headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods' : 'GET',
-          'Access-Control-Allow-Headers' : 'Origin, Content-Type, X-Auth-Token'
-      }
-    };
-
-    var request = http.request(options, function(result){
-
-      var output = '';
-
-      result.on('data', function (chunk) {
-          output += chunk;
+      resp.on('data', (chunk) => {
+        data += chunk;
       });
 
-      result.on('end', function() {
-          res.status(200).send(output);
+      resp.on('end', () => {
+        res.status(200).send(data);
       });
 
-    })
-
-    request.on('error', function(err){
+    }).on("error", (err) => {
       res.status(400).send(err);
     });
-
-    request.end();*/
   }
 }
