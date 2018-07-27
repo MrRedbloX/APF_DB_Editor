1cd6005ca19f20c47f21212f598811b08d0368d6
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-27 17:43:11 +0200

    2018-07-27 17:43:10

--- public/js/controllers/providersControllers.js
+++ public/js/controllers/providersControllers.js
@@ -9,20 +9,28 @@ app.controller('mainProvidersController', function($scope){
   $scope.checkProvider = function(){
     $scope.selectedProvider = ((window.location.href.split('?')[1]).split('&')[0]).split('=')[1];
   };
+
+  $scope.queryTenant = function(provider){
+
+  };
 });
 
 app.controller('awsProviderController', function($scope){
   $scope.controller = "AWS";
+  $scope.tenants = [];
 });
 
 app.controller('azureProviderController', function($scope){
   $scope.controller = "Azure";
+  $scope.tenants = [];
 });
 
 app.controller('fcaProviderController', function($scope){
   $scope.controller = "FCA";
+  $scope.tenants = [];
 });
 
 app.controller('feProviderController', function($scope){
   $scope.controller = "Flexible Engine";
+  $scope.tenants = [];
 });
