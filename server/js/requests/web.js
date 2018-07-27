24f3d977267172e092fc5909d727ac3728039d06
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-27 10:35:38 +0200

    2018-07-27 10:35:38

--- server/js/requests/web.js
+++ server/js/requests/web.js
@@ -1,5 +1,6 @@
 var http = require("http");
 module.exports = {
+  //Query Orange calendar to find if a user who wants to register is in the calendar
   getAnnuaire: function(req, res){
     lien = req.query.lien+"&personCriteria.sn="+req.query.personCriteriaN+"&personCriteria.givenName="+req.query.personCriteriaP+"&personCriteria.mail="+req.query.personCriteriaM;
 
