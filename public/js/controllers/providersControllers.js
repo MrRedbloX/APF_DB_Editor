b21d0f21ac089812c7f6e08afb7401ce00699014
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-27 17:20:17 +0200

    2018-07-27 17:20:17

--- public/js/controllers/providersControllers.js
+++ public/js/controllers/providersControllers.js
@@ -1,6 +1,32 @@
 app.controller('mainProvidersController', function($scope){
-  $scope.checkProvider = function(){
-    console.log(((window.location.href.split('?')[1]).split('&')[0]).split('=')[1]);
+  $scope.selectedProvider = null;
 
+  $scope.checkProvider = function(){
+    let provider = ((window.location.href.split('?')[1]).split('&')[0]).split('=')[1];
+
+    if(provider == "AWS")
+      selectedProvider = "awsProviderController";
+    else if(provider == "Azure")
+      selectedProvider = "azureProviderController";
+    else if(provider == "FCA")
+      selectedProvider = "fcaProviderController";
+    else if(provider == "FE")
+      selectedProvider = "feProviderController";
   };
+});
+
+app.controller('awsProviderController', function($scope){
+  $scope.controller = "AWS";
+});
+
+app.controller('azureProviderController', function($scope){
+  $scope.controller = "Azure";
+});
+
+app.controller('fcaProviderController', function($scope){
+  $scope.controller = "FCA";
+});
+
+app.controller('feProviderController', function($scope){
+  $scope.controller = "Flexible Engine";
 });
