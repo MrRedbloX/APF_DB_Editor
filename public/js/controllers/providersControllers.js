30484b06ac3fb0bba8e8167b0026e36c96752098
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-27 17:09:49 +0200

    2018-07-27 17:09:49

--- public/js/controllers/providersControllers.js
+++ public/js/controllers/providersControllers.js
@@ -1,5 +1,5 @@
 app.controller('mainProvidersController', function($scope){
   $scope.checkProvider = function(){
-    console.log(window.location.href.split('?'));
+    console.log((window.location.href.split('?')[1]).split('&'));
   };
 });
