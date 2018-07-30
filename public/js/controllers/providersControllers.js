7492335e465cf106405bdc359d0c3ffb43914cb9
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-30 18:19:06 +0200

    2018-07-30 18:19:06

--- public/js/controllers/providersControllers.js
+++ public/js/controllers/providersControllers.js
@@ -221,7 +221,6 @@ app.controller('mainProvidersController', function($scope, mainProvidersFactory,
     let values = [];
     postgresScope.query($scope.database, $scope.kp_table, "*", "tenant_uuid", $scope.selectedTenantID, function(){
       if(postgresScope.successRequest){
-        console.log(postgresScope.queryRequest.data);
         for(let i=0; i<postgresScope.queryRequest.data.length; i++){
           values.push({
             id : postgresScope.queryRequest.data[i].uuid,
