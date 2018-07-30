56dcf4910772a9f3b777b9675c9da9d82e2a2940
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-30 17:58:54 +0200

    2018-07-30 17:58:54

--- public/js/controllers/providersControllers.js
+++ public/js/controllers/providersControllers.js
@@ -122,7 +122,7 @@ app.controller('mainProvidersController', function($scope, mainProvidersFactory,
       if(postgresScope.successRequest){
         for(let i=0; i<postgresScope.queryRequest.data.length; i++){
           if(postgresScope.queryRequest.data[i].tenant_region == split[1]){
-
+            console.log("Ressources query");
             await $scope.queryRessources("ECS");
             /*await $scope.queryRessources("VPC");
             await $scope.queryRessources("SG");
