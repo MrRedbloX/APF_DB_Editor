070a93712ab9541182155363ca6b70fe18f175b0
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-30 10:18:02 +0200

    2018-07-30 10:18:02

--- public/js/controllers/providersControllers.js
+++ public/js/controllers/providersControllers.js
@@ -8,12 +8,30 @@ app.controller('mainProvidersController', function($scope, mainProvidersFactory,
   $scope.fcaProvider = "FCA";
   $scope.feProvider = "FE";
 
+  $scope.database = "sonde";
+  $scope.tenant_table = "tenant_table";
+  $scope.provider_table = "provider_table";
+  $scope.foreign_key = "provider_uuid";
+
   $scope.checkProvider = function(){
     $scope.selectedProvider = ((window.location.href.split('?')[1]).split('&')[0]).split('=')[1];
+
+    queryVar = $scope.selectedProvider;
+    if($scope.selectedProvider == $scope.feProvider) queryVar = "Flexible Engine";
+
+    postgresScope.query($scope.database, $scope.provider_table, "uuid", "name", queryVar, function(){
+      if(postgresScope.successRequest){
+
+      }
+      else{
+        console.log(postgresScope.queryRequest);
+        alert("Error on query request, check console logs.");
+      }
+    });
   };
 
   $scope.queryTenants = function(provider){
-
+    postgresScope.query($scope.database, $scope.tenant_table, "*", )
   };
 });
 
