c4e65a05c89f72e8349db06a2a71411cc3ee3b59
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-31 11:24:03 +0200

    2018-07-31 11:24:03

--- public/js/controllers/providersControllers.js
+++ public/js/controllers/providersControllers.js
@@ -169,12 +169,13 @@ app.controller('mainProvidersController', function($scope, mainProvidersFactory,
       if(postgresScope.successRequest){
         for(let i=0; i<postgresScope.queryRequest.data.length; i++){
           //let add = "";
-          await $scope.queryObjects("Subnet", postgresScope.queryRequest.data[i].uuid, postgresScope.queryRequest.data[i].vpc_name);
+
           //if ($scope.objects[postgresScope.queryRequest.data[i].vpc_name] != null && $scope.objects[postgresScope.queryRequest.data[i].vpc_name].length > 0) add = "subnet(s)";
           values.push({
             id : postgresScope.queryRequest.data[i].uuid,
             name : postgresScope.queryRequest.data[i].vpc_name
           });
+          await $scope.queryObjects("Subnet", postgresScope.queryRequest.data[i].uuid, postgresScope.queryRequest.data[i].vpc_name);
           if(i == postgresScope.queryRequest.data.length-1){
             $scope.ressources.push({
               name : "Virtual Private Cloud(s)",
