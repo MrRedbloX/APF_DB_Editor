67d00a511ff46ffde7692ebc38c50e867b6a9144
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-31 15:40:17 +0200

    2018-07-31 15:40:17

--- public/js/controllers/providersControllers.js
+++ public/js/controllers/providersControllers.js
@@ -85,9 +85,6 @@ app.controller('mainProvidersController', function($scope, mainProvidersFactory,
       try{
         let treeView = $("#"+id);
         treeView.jstree()
-        .on('ready.jstree', function(){
-          treeView.jstree('open_all');
-        })
         .on('select_node.jstree', function(e, data){
           if(mode == "Tenant") $scope.getRessources(data.node.text);
         });
