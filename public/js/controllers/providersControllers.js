b1070897267d58906ea4dee399af035a08571728
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-31 15:43:14 +0200

    2018-07-31 15:43:14

--- public/js/controllers/providersControllers.js
+++ public/js/controllers/providersControllers.js
@@ -273,7 +273,6 @@ app.controller('mainProvidersController', function($scope, mainProvidersFactory,
                   name : postgresScope.queryRequest.data[k].subnet_name
                 });
               }
-              if(values.length > 0 ) $scope.ressources[i].values[j].name = $scope.ressources[i].values[j].name+" subnet(s)";
               $scope.objects[$scope.ressources[i].values[j].name] = values;
               resolve();
             }
