a3987e364b1a0daa2cf3913b23711030e8cd2d29
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-31 15:06:29 +0200

    2018-07-31 15:06:28

--- public/js/controllers/providersControllers.js
+++ public/js/controllers/providersControllers.js
@@ -274,9 +274,9 @@ app.controller('mainProvidersController', function($scope, mainProvidersFactory,
                 });
               }
               //if(values.length > 0 ) $scope.ressources[i].values[j].name = $scope.ressources[i].values[j].name+" subnet(s)";
-              $scope.objects.push({
-                  $scope.ressources[i].values[j].name : values
-              });
+              let obj = {};
+              obj[$scope.ressources[i].values[j].name] = values;
+              $scope.objects.push(obj);
               resolve();
             }
             else{
