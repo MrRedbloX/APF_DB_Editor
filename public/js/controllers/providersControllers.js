818cb8419aad6669cfda97ace2a9039dcbd74812
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-07-31 17:45:58 +0200

    2018-07-31 17:45:57

--- public/js/controllers/providersControllers.js
+++ public/js/controllers/providersControllers.js
@@ -97,6 +97,7 @@ app.controller('mainProvidersController', function($scope, $route, mainProviders
   $scope.loadJSTree= function(id){
     $(function() {
       let treeView = $("#"+id);
+      sleep(50);
       treeView.jstree();
     });
   }
