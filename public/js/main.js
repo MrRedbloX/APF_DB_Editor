1edc3dbe7339abd353bb0858c045b820f36169a0
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 11:59:11 +0200

    2018-08-21 11:59:11

--- public/js/main.js
+++ public/js/main.js
@@ -33,7 +33,7 @@ var getValueOfVar = function(name){
   else alert("Error on request init file, please contact your administrator");
   return ret;
 }
-console.log(getValueOfVar('busy'));
+console.log(getValueOfVar('forbiddenChar'));
 
 var exceptionDB = ['postgres', 'template0', 'template1']; //The databases that will not be displayed
 var exceptionColumns = ['uuid']; //The columns that will not be displayed
