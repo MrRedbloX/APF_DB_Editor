2027bf18a5ff41245d3ddf5ab112ae41a61a7705
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 14:49:48 +0200

    2018-08-21 14:49:48

--- public/js/controllers/configurationControllers.js
+++ public/js/controllers/configurationControllers.js
@@ -1,8 +1,15 @@
 app.controller('configurationController', function($scope){
   $scope.select = "select";
   $scope.input = "input";
+  $scope.list = "list";
 
   $scope.variables = [
+    {
+      name: "exceptionDB",
+      description: "The database(s) not displayed in DB Management and Dashboard",
+      type: "list",
+      value : exceptionDB
+    },
     {
       name: "waitFor",
       description: "The time in ms the application will wait to prevent from network issues is : ",
