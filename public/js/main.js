d5577bbcb341209e8ef8a0a0e73c1da33593f1b5
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 11:36:48 +0200

    2018-08-21 11:36:48

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,3 @@
-var exceptionDB = ['postgres', 'template0', 'template1']; //The databases that will not be displayed
-var exceptionColumns = ['uuid']; //The columns that will not be displayed
-var readOnlyDB = ['sonde']; //Contains the read only databases
-var displayName = ['name', 'tenant_name', 'sg_name', 'subnet_name', 'ecs_name', 'kp_name', 'vpc_name', 'uuid']; //The name that will be displayed instead of the id
-var busy = false; //When a view in db_management is displayed, this turn to true in order to prevent from some unwanted behaviors
-var exceptionTables = ['map']; //The tables which starts with these elements won't be displayed in the dashboard charts
-var waitFor = 1; //The time in ms use in the sleep function
-var forbiddenChar = ['#','%','&','+','[',']','{','}',"'",'"','\\']; //The chars that the user can't write when he add or modify an element
-var isAdmin = false;
-
 var getValuesOfVar = function(){
   let ret;
   $.ajax({
@@ -20,7 +10,21 @@ var getValuesOfVar = function(){
   });
   return ret;
 };
-console.log(getValuesOfVar());
+var config = getValuesOfVar();
+var getValueOfVar = function(){
+  return config.split('\n');
+}
+console.log(getValuesOf());
+
+var exceptionDB = ['postgres', 'template0', 'template1']; //The databases that will not be displayed
+var exceptionColumns = ['uuid']; //The columns that will not be displayed
+var readOnlyDB = ['sonde']; //Contains the read only databases
+var displayName = ['name', 'tenant_name', 'sg_name', 'subnet_name', 'ecs_name', 'kp_name', 'vpc_name', 'uuid']; //The name that will be displayed instead of the id
+var busy = false; //When a view in db_management is displayed, this turn to true in order to prevent from some unwanted behaviors
+var exceptionTables = ['map']; //The tables which starts with these elements won't be displayed in the dashboard charts
+var waitFor = 1; //The time in ms use in the sleep function
+var forbiddenChar = ['#','%','&','+','[',']','{','}',"'",'"','\\']; //The chars that the user can't write when he add or modify an element
+var isAdmin = false;
 
 //Check if a table is elligible to exceptionTables
 var isInExceptionTables = function(table){
