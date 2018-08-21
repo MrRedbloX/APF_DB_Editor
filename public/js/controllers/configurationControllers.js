f6c81d28ab9e578b811fb7136f15646cb1720aea
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 15:13:53 +0200

    2018-08-21 15:13:53

--- public/js/controllers/configurationControllers.js
+++ public/js/controllers/configurationControllers.js
@@ -14,31 +14,31 @@ app.controller('configurationController', function($scope){
       name: "exceptionColumns",
       description: "The column(s) not used to add/modify a record : ",
       type: "list",
-      value : exceptionDB
+      value : exceptionColumns
     },
     {
       name: "readOnlyDB",
       description: "The database(s) in read only mode : ",
       type: "list",
-      value : exceptionDB
+      value : readOnlyDB
     },
     {
       name: "displayName",
       description: "The list of names displayed in foreign keys instead of the ID : ",
       type: "list",
-      value : exceptionDB
+      value : displayName
     },
     {
       name: "exceptionTables",
       description: "The tables which start with the following element(s) will not be displayed in Dashboard : ",
       type: "list",
-      value : exceptionDB
+      value : exceptionTables
     },
     {
       name: "forbiddenChar",
       description: "The forbidden character(s) when the user wants to add/modify a record : ",
       type: "list",
-      value : exceptionDB
+      value : forbiddenChar
     },
     {
       name: "waitFor",
