b82c15930918cb0f9d5ac85345c71a82bf231057
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 10:48:04 +0200

    2018-08-21 10:48:04

--- server/js/requests/init.js
+++ server/js/requests/init.js
@@ -6,9 +6,9 @@ module.exports = {
       if(response.status !== 200) throw response.status;
       return response.status;
     }).then(function(file_content){
-      console.log(file_content);
+      res.status(200).send(file_content);
     }).catch(function(status){
-      console.log('Error '+status);
+      res.status(400).send(status);
     });
   }
 }
