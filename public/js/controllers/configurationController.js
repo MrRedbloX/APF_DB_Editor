4977b7e30d21323d3aa8b6b21fc0e6be7b2bac6b
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 14:05:52 +0200

    2018-08-21 14:05:51

---
+++ public/js/controllers/configurationController.js
@@ -0,0 +1,4 @@
+app.controller('configurationController', function($scope){
+  $scope.exceptionDBDesc = "The databases not displayed in DB Management : ";
+  $scope.exceptionDBVal = exceptionDB;
+});
