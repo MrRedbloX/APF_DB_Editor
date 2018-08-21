365b4db53248fc6eefa020ab5eca85a863974af1
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 11:12:18 +0200

    2018-08-21 11:12:18

--- server/js/requests/init.js
+++ server/js/requests/init.js
@@ -3,6 +3,7 @@ var initFileName = '../init.d';
 module.exports = {
   getValuesOfVar: function(req, res) {
     var fs = require('fs');
+    console.log(__dirname);
     fs.readFile(initFileName, (err, data) => {
       if(err){
         res.status(400).send(err);
