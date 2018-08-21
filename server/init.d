85b09bc7bc5df363c55ab972c0453853f04eda6f
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 10:31:14 +0200

    2018-08-21 10:31:14

---
+++ server/init.d
@@ -0,0 +1,6 @@
+exceptionDB:array:postgres,template0,template1;
+exceptionColumns:array:uuid;
+readOnlyDB:array:sonde;
+displayName:array:name,tenant_name,sg_name,subnet_name,ecs_name,kp_name,vpc_name,uuid;
+busy:bool:false;
+exceptionTables:array:map;
