1f327f82608af0b5e9144f8ee0877a96036b7b42
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 14:21:25 +0200

    2018-08-21 14:21:25

--- server/init.d
+++ server/init.d
@@ -2,7 +2,6 @@ exceptionDB:array:postgres,template0,template1;
 exceptionColumns:array:uuid;
 readOnlyDB:array:sonde;
 displayName:array:name,tenant_name,sg_name,subnet_name,ecs_name,kp_name,vpc_name,uuid;
-busy:bool:false;
 exceptionTables:array:map;
 waitFor:int:1;
 forbiddenChar:array:#,%,&,+,[,],{,},',",\;
