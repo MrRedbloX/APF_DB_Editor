04538feab6cf67794f39671f4354dabb0f770df0
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 11:55:37 +0200

    2018-08-21 11:55:37

--- public/js/main.js
+++ public/js/main.js
@@ -20,6 +20,11 @@ var getValueOfVar = function(name){
       if(listVar2[1] == 'array') ret = listVar2[2].split(',');
       else if(listVar2[1] == 'string') ret = listVar2[2];
       else if(listVar2[1] == 'int') ret = parseInt(listVar2[2]);
+      else if(listVar2[1] == 'int'){
+        if(listVar2[2] == 'true') ret = true;
+        else ret = false;
+      }
+      else console.log("Unhandle type");
       break;
     }
   }
