44a8571b0c6d9d0dc593f229f31fc9747801cf1a
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 11:14:27 +0200

    2018-08-21 11:14:27

--- server/js/requests/init.js
+++ server/js/requests/init.js
@@ -1,15 +1,14 @@
-var initFileName = '../init.d';
+var initFileName = '../../init.d';
 
 module.exports = {
   getValuesOfVar: function(req, res) {
-    var fs = require('fs');
-    console.log(__dirname);
-    /*fs.readFile(initFileName, (err, data) => {
+    var fs = require('fs')
+    fs.readFile(initFileName, (err, data) => {
       if(err){
         res.status(400).send(err);
         throw err;
       }
       res.status(200).send(data);
-    });*/
+    });
   }
 }
