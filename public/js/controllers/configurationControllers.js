7ad9853cd33984523f696ccc803894393c7be064
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 14:36:42 +0200

    2018-08-21 14:36:42

--- public/js/controllers/configurationControllers.js
+++ public/js/controllers/configurationControllers.js
@@ -1,13 +1,13 @@
 app.controller('configurationController', function($scope){
   $scope.select = "select";
-  $scope.int = "int";
-  
+  $scope.input = "input";
+
   $scope.variables = [
     {
       name: "waitFor",
       description: "The time in ms the application will wait to prevent from network issues is : ",
-      type : "int",
-      value : 1
+      type : "input",
+      value : waitFor
     }
   ];
 });
