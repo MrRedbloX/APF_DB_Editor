fd0948d26272c5499d45f56b0662490e9b1cff2f
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-21 10:32:45 +0200

    2018-08-21 10:32:45

--- server/init.d
+++ server/init.d
@@ -4,3 +4,5 @@ readOnlyDB:array:sonde;
 displayName:array:name,tenant_name,sg_name,subnet_name,ecs_name,kp_name,vpc_name,uuid;
 busy:bool:false;
 exceptionTables:array:map;
+waitFor:int:1;
+forbiddenChar:array:#,%,&,+,[,],{,},',",\;
