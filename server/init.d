590f7b75c0c33e897255dbe1b16be40c8d9454e0
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-22 14:34:51 +0200

    2018-08-22 14:34:51

--- server/init.d
+++ server/init.d
@@ -1,1 +1,1 @@
-exceptionDB:list:postgres,template0,template1;exceptionColumns:list:uuid;readOnlyDB:list:sonde;displayName:list:name,tenant_name,sg_name,subnet_name,ecs_name,kp_name,vpc_name,uuid;exceptionTables:list:map;waitFor:int:1;forbiddenChar:list:#,%,&,+,[,],{,},',",\;
+exceptionDB:list:postgres,template0,template1;exceptionColumns:list:uuid;readOnlyDB:list:sonde;displayName:list:name,tenant_name,sg_name,subnet_name,ecs_name,kp_name,vpc_name,uuid;exceptionTables:list:map;waitFor:int:1;
