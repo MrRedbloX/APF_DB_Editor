53df97a9b328a889bfa7c375972748467d7ff442
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-22 12:51:42 +0200

    2018-08-22 12:51:41

--- server/js/requests/init.js
+++ server/js/requests/init.js
@@ -14,6 +14,7 @@ module.exports = {
 
   writeInInitFile: function(req, res) {
     var fs = require('fs');
+    console.log(req.query.content);
     fs.writeFile(initFileName, req.query.content, (err, data) => {
       if(err){
         res.status(400).send(err);
