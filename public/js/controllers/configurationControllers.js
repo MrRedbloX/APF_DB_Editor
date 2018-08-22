ada25459ff2055b4a8941264e6bd9450cef09285
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-22 12:25:43 +0200

    2018-08-22 12:25:43

--- public/js/controllers/configurationControllers.js
+++ public/js/controllers/configurationControllers.js
@@ -44,6 +44,7 @@ app.controller('configurationController', function($scope){
       name: "waitFor",
       description: "The time in ms the application will wait to prevent from network issues is : ",
       type : "input",
+      typeBis: 'int',
       value : waitFor
     }
   ];
