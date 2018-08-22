e1fe5a015b1d9ea132c638f21144bd9f2f4315d7
MrRedbloX <32101242+MrRedbloX@users.noreply.github.com>
2018-08-22 12:18:50 +0200

    2018-08-22 12:18:50

--- public/js/main.js
+++ public/js/main.js
@@ -53,7 +53,7 @@ var alterInitFile = function(newConfig){
     else str += newConfig[i].value;
     str += ';\n';
   }
-  writeFile(str);
+  writeInInitFile(str);
 };
 var exceptionDB = getValueOfVar('exceptionDB'); //The databases that will not be displayed
 var exceptionColumns = getValueOfVar('exceptionColumns'); //The columns that will not be displayed
